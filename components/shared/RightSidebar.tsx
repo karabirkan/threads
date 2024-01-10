612dba0f5d0d6976ca2fc6bda5f36b654e3cb9fa
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2024-01-10 15:02:13 +0300

    feat: Bottom bar for mobile nav

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3>Suggested Communities</h3>
+      </div>
+    </section>
+  );
 };
 
 export default RightSidebar;
