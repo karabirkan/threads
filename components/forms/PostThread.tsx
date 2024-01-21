b5172c1a5443ba7741ea4ce67c734464526db285
tokyoparis <birkannewyork@gmail.com>
2024-01-21 22:38:39 +0300

    feat: create thread page

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostThread = ({ userId }: { userId: String }) => {
+    return (
+        <div>
+            Post Thread Form
+        </div>
+    )
+}
+
+export default PostThread
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -38,3 +38,16 @@ export async function updateUser({
     throw new Error(`Failed to create/update user: ${error.message}`);
   }
 }
+
+export async function fetchUser(userId: string) {
+  connectToDB();
+  try {
+    return await User.findOne({ id: userId });
+    // .populate({
+    //   path:"communities",
+    //   model:"Community"
+    // })
+  } catch (error) {
+    console.log(error);
+  }
+}
