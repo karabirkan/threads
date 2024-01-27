b0a69c05db59ad3176d1943f99a6d1576df95d86
tokyoparis <birkannewyork@gmail.com>
2024-01-27 19:36:43 +0300

    feat: comment section

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
 import { fetchPosts, fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
@@ -8,20 +9,21 @@ import React from "react";
 const page = async ({ params }: { params: { id: string } }) => {
   if (!params.id) return null;
 
-  const user = currentUser();
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
 
-  if (userInfo?.onboarded) redirect("/onboarding");
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const thread = await fetchThreadById(params.id);
+
   return (
     <section className="relative">
       <div>
         <ThreadCard
           id={thread._id}
-          currentUserId={thread?.id || ""}
+          currentUserId={user?.id || ""}
           parentId={thread.parentId}
           key={thread._id}
           content={thread.text}
@@ -29,6 +31,13 @@ const page = async ({ params }: { params: { id: string } }) => {
           community={thread.community}
           createdAt={thread.created}
           comments={thread.children}
+        />
+      </div>
+      <div className="mt-7">
+        <Comment
+          threadId={thread.id}
+          currentUserImg={user.imageUrl}
+          currentUserId={JSON.stringify(userInfo._id)}
         />
       </div>
     </section>
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -102,10 +102,40 @@ export async function fetchThreadById(id: string) {
             },
           },
         ],
-      });
+      })
+      .exec();
 
     if (!thread) return null;
     return thread;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+  try {
+    const originalThread = await Thread.findById(threadId);
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+    const savedCommentThread = await commentThread.save();
+
+    originalThread.children.push(savedCommentThread._id);
+
+    await originalThread.save();
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
   }
